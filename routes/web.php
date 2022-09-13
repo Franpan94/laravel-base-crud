c8d052d2f4d666befc90faf1c098d9fa229a5e7d
Franpan94 <francescopanico94@libero.it>
2022-09-13 16:54:48 +0200

    layout of the array

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+Route::get('/comics', 'ComicController@index');
+Route::get('/comics', 'ComicController@show');
+    
+
