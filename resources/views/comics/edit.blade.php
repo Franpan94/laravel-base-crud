abcab4c726d503711c1dd14a35e0287696de7cb4
Franpan94 <francescopanico94@libero.it>
2022-09-16 16:50:01 +0200

    validation edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -6,37 +6,65 @@
         @method('PUT')
 
         <div class="p-4">Titolo <br>
-            <input type="text" name="title" id="title" placeholder="Inserisci titolo" value="{{ $comic->title }}"
-                required>
+            <input type="text" name="title" id="title" placeholder="Inserisci titolo" 
+              value="{{ old('title', $comic->title) }}" required 
+            >
+            @error('title')
+              <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="p-4">Descrizione <br>
             <textarea name="description" id="description" cols="60" rows="20" placeholder="Inserisci descrizione"
-                required>{{ $comic->description }}
+                required>{{ old('description', $comic->description) }}
             </textarea>
+            @error('description')
+              <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="p-4">Copertina <br>
-            <textarea name="thumb" id="thumb" cols="30" rows="10" placeholder="Inserisci copertina" required>{{ $comic->thumb }}
+            <textarea name="thumb" id="thumb" cols="30" rows="10" placeholder="Inserisci copertina" 
+              required>{{ old('thumb', $comic->thumb) }}
             </textarea>
+            @error('thumb')
+              <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="p-4">Prezzo <br>
-            <input type="text" id="price" name="price" placeholder="Inserisci prezzo" value="{{ $comic->price }}"
-                required>
+            <input type="text" id="price" name="price" placeholder="Inserisci prezzo" 
+              value="{{ old('price', $comic->price) }}" required
+            >
+            @error('price')
+              <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="p-4">Serie <br>
-            <input type="text" id="series" name="series" placeholder="Inserisci serie" value="{{ $comic->series }}"
-                required>
+            <input type="text" id="series" name="series" placeholder="Inserisci serie" 
+              value="{{ old('series', $comic->series) }}" required
+            >
+            @error('series')
+              <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="p-4">Data <br>
-            <input type="date" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}" required>
+            <input type="date" id="sale_date" name="sale_date" 
+             value="{{ old('sale_date', $comic->sale_date) }}"required
+            >
+            @error('sale_date')
+              <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="p-4">Tipo <br>
-            <input type="text" id="type" name="type" placeholder="Inserisci tipo" value="{{ $comic->type }}"
-                required>
+          <input type="text" id="type" name="type" placeholder="Inserisci tipo" 
+            value="{{ old('type', $comic->type) }}" required
+          >
+          @error('type')
+            <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
         <div class="p-4">
-            <button class="btn btn-primary" type="submit">
-                Invia
-            </button>
+          <button class="btn btn-primary" type="submit">
+            Invia
+          </button>
         </div>
     </form>
 @endsection
